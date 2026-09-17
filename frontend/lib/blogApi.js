f83b23a3d22c748b86e26dc6365850ccdf1c4e43
Rajkumar Aryal <80311912+imrajkumar-git@users.noreply.github.com>
2026-09-17 21:31:07 +0400

    Fix blog API URL by removing extra slash

--- frontend/lib/blogApi.js
+++ frontend/lib/blogApi.js
@@ -1,7 +1,7 @@
 import { createClient } from "./httpClient";
 
 const BLOG_API_URL =
-  process.env.NEXT_PUBLIC_BLOG_API_URL || "https://backend-blogs-sspm.onrender.com//api/blog";
+  process.env.NEXT_PUBLIC_BLOG_API_URL || "https://backend-blogs-sspm.onrender.com/api/blog";
 
 const blogApi = createClient(BLOG_API_URL);
 
