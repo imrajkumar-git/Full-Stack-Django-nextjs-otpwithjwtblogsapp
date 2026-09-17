b250d0da6e232f81ebaf1aa6370674b3edd8b77c
Rajkumar Aryal <80311912+imrajkumar-git@users.noreply.github.com>
2026-09-17 21:28:15 +0400

    Update BLOG_API_URL to use production endpoint

--- frontend/lib/blogApi.js
+++ frontend/lib/blogApi.js
@@ -1,7 +1,7 @@
 import { createClient } from "./httpClient";
 
 const BLOG_API_URL =
-  process.env.NEXT_PUBLIC_BLOG_API_URL || "http://192.168.0.120:8000/api/blog";
+  process.env.NEXT_PUBLIC_BLOG_API_URL || "https://backend-blogs-sspm.onrender.com//api/blog";
 
 const blogApi = createClient(BLOG_API_URL);
 
